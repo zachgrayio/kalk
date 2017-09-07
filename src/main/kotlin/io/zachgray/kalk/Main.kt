42582f079cd3f3805c66bfc306e0e79abaeb56b7
Zach Gray <zgray@scal.io>
2017-09-07 02:10:02 -0700

    join args to string so spaces are supported in expression passed to cli program. fix warning.

--- src/main/kotlin/io/zachgray/kalk/Main.kt
+++ src/main/kotlin/io/zachgray/kalk/Main.kt
@@ -4,7 +4,7 @@ import example.math.ext.toRPNExpression
 import io.zachgray.cmdloop.commandLoop
 
 fun main(args:Array<String>) {
-    if(!args.isEmpty()) return println(args.first().toRPNExpression().evaluate())
+    if(!args.isEmpty()) return println(args.joinToString(" ").toRPNExpression().evaluate())
     commandLoop {
         welcomeMessage {
             "Hi! Enter a mathematical expression to be evaluated, or enter a command."
@@ -21,7 +21,7 @@ fun main(args:Array<String>) {
             }
         }
         catch {
-            { e ->
+            { _ ->
                 println("  invalid expression, please try again.")
             }
         }
