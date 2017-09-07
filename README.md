6e79d3b6e457c2c49b6a238d994d86905cd8c984
Zach Gray <zgray@scal.io>
2017-09-07 02:05:44 -0700

    intial implementation of shunting yard parsing and RPN evaluation in kotlin and interactive program via cmdloop

---
+++ README.md
@@ -0,0 +1,12 @@
+# kalk
+
+A CLI calculator written in Kotlin.
+
+### Installation
+
+`npm i -g kalk` - release coming soon!
+
+### Usage
+
+- `kalk` for interactive mode
+- `kalk 2 + 2` to verify two plus two is in fact four!
---
+++ src/main/kotlin/io/zachgray/kalk/Main.kt
@@ -0,0 +1,29 @@
+package io.zachgray.kalk
+
+import example.math.ext.toRPNExpression
+import io.zachgray.cmdloop.commandLoop
+
+fun main(args:Array<String>) {
+    if(!args.isEmpty()) return println(args.first().toRPNExpression().evaluate())
+    commandLoop {
+        welcomeMessage {
+            "Hi! Enter a mathematical expression to be evaluated, or enter a command."
+        }
+        commandPrefix {
+            "/"
+        }
+        default {
+            { input ->
+                input?.let {
+                    val expression = it.toRPNExpression()
+                    println("  ${expression.evaluate()}")
+                }
+            }
+        }
+        catch {
+            { e ->
+                println("  invalid expression, please try again.")
+            }
+        }
+    }
+}
