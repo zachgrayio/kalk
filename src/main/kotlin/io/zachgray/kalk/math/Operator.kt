c3e8711157d1f6288db435045abc77797aa79e13
Zach Gray <zgray@scal.io>
2017-09-08 20:38:43 -0700

    implement comparison ops

--- src/main/kotlin/io/zachgray/kalk/math/Operator.kt
+++ src/main/kotlin/io/zachgray/kalk/math/Operator.kt
@@ -9,35 +9,60 @@ sealed class Operator(val stringRepresentation:String, val precedence:Int, val i
     abstract fun operate(right:Double, left:Double):Double
 
     // 1
-    object plus : Operator(stringRepresentation = "+", precedence = 0, isRightAssociative = false) {
+    object eq : Operator(stringRepresentation = "=", precedence = 0, isRightAssociative = false) {
+        override fun operate(right: Double, left: Double): Double = if(left == right) 1.0 else 0.0
+    }
+
+    object leq : Operator(stringRepresentation = "<=", precedence = 0, isRightAssociative = false) {
+        override fun operate(right: Double, left: Double): Double = if(left <= right) 1.0 else 0.0
+    }
+
+    object geq : Operator(stringRepresentation = ">=", precedence = 0, isRightAssociative = false) {
+        override fun operate(right: Double, left: Double): Double = if(left >= right) 1.0 else 0.0
+    }
+
+    object lt : Operator(stringRepresentation = "<", precedence = 0, isRightAssociative = false) {
+        override fun operate(right: Double, left: Double): Double = if(left < right) 1.0 else 0.0
+    }
+
+    object gt : Operator(stringRepresentation = ">", precedence = 0, isRightAssociative = false) {
+        override fun operate(right: Double, left: Double): Double = if(left > right) 1.0 else 0.0
+    }
+
+    object plus : Operator(stringRepresentation = "+", precedence = 1, isRightAssociative = false) {
         override fun operate(right: Double, left: Double) = left.plus(right)
     }
 
-    object minus : Operator(stringRepresentation = "-", precedence = 0, isRightAssociative = false) {
+    object minus : Operator(stringRepresentation = "-", precedence = 1, isRightAssociative = false) {
         override fun operate(right: Double, left: Double) = left.minus(right)
     }
 
-    object times : Operator(stringRepresentation = "*", precedence = 1, isRightAssociative = false) {
+    object times : Operator(stringRepresentation = "*", precedence = 2, isRightAssociative = false) {
         override fun operate(right: Double, left: Double) = left.times(right)
     }
 
-    object div : Operator(stringRepresentation = "/", precedence = 1, isRightAssociative = false) {
+    object div : Operator(stringRepresentation = "/", precedence = 2, isRightAssociative = false) {
         override fun operate(right: Double, left: Double) = left.div(right)
     }
 
-    object mod : Operator(stringRepresentation = "%", precedence = 1, isRightAssociative = false) {
+    object mod : Operator(stringRepresentation = "%", precedence = 2, isRightAssociative = false) {
         override fun operate(right: Double, left: Double) = left.rem(right)
     }
 
-    object pow : Operator(stringRepresentation = "^", precedence = 2, isRightAssociative = true) {
+    object pow : Operator(stringRepresentation = "^", precedence = 3, isRightAssociative = true) {
         override fun operate(right: Double, left: Double) = Math.pow(left, right)
     }
 
     companion object {
         // 2
-        val tokens = "\\^%*+\\-"
+        val tokens = "><=\\^%*+\\-"
 
         fun fromString(representation:String) = when(representation) {
+            eq.stringRepresentation -> eq
+            leq.stringRepresentation -> leq
+            geq.stringRepresentation -> geq
+            lt.stringRepresentation -> lt
+            gt.stringRepresentation -> gt
             plus.stringRepresentation -> plus
             minus.stringRepresentation -> minus
             times.stringRepresentation -> times
@@ -48,7 +73,5 @@ sealed class Operator(val stringRepresentation:String, val precedence:Int, val i
         }
     }
 
-    override fun toString(): String {
-        return stringRepresentation
-    }
+    override fun toString(): String = stringRepresentation
 }
