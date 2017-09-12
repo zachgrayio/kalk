f03142ea2418c03c15103749216356e3497de634
Zach Gray <zgray@scal.io>
2017-09-11 18:35:55 -0700

    implement random command

--- src/main/kotlin/io/zachgray/kalk/Main.kt
+++ src/main/kotlin/io/zachgray/kalk/Main.kt
@@ -2,6 +2,7 @@ package io.zachgray.kalk
 
 import io.zachgray.kalk.math.ext.toRPNExpression
 import io.zachgray.cmdloop.commandLoop
+import java.util.*
 
 fun main(args:Array<String>) {
     if(!args.isEmpty()) return println(args.joinToString(" ").toRPNExpression().evaluate())
@@ -11,6 +12,26 @@ fun main(args:Array<String>) {
         }
         commandPrefix {
             "/"
+        }
+        command("random") { params, _ ->
+            try {
+                val intParams = params
+                    .map { it.toIntOrNull() }
+                    .filter { it != null }
+                    .map { it!! }
+                val min = when {
+                    intParams.size == 2 -> intParams.first()
+                    else -> 1
+                }
+                val max = when {
+                    intParams.size == 2 -> intParams.last()
+                    intParams.size == 1 -> intParams.first()
+                    else -> 100
+                }
+                println("  ${Random().nextInt(max + 1 - min).plus(min)}")
+            } catch(t:Throwable) {
+                println("  invalid parameters. usage: /random 1 10")
+            }
         }
         default {
             { input ->
