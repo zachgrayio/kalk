359859b1d76a7113ee8ed4ad3cc3fab41d799e66
Zach Gray <zgray@scal.io>
2017-09-11 17:13:10 -0700

    refactor Operator file to make more use of sealed classes

--- src/main/kotlin/io/zachgray/kalk/math/Operator.kt
+++ src/main/kotlin/io/zachgray/kalk/math/Operator.kt
@@ -2,91 +2,21 @@ package io.zachgray.kalk.math
 
 /**
  * Supported operators. To add one:
- * 1: add an Operator entry which implements `operate()`
- * 2: add the token to the `tokens` val on the companion object.
+ * 1: add the token to the `tokens` val on the companion object.
+ * 2: add an entry to the `fromString` when expression so that operator string token can be mapped to the Operator
+ * 3: add an Operator entry with operator metadata
+ * 4: expand `operate` function with operator impl
  */
 sealed class Operator(
     val stringRepresentation:String,
     val precedence:Int,
-    val isRightAssociative:Boolean,
-    val operateFunc: (left: Double, right: Double) -> Double
+    val isRightAssociative:Boolean
 ) {
-    fun operate(right:Double, left:Double):Double = operateFunc(left, right)
-
-    // 1
-    object eq : Operator(
-        stringRepresentation = "=",
-        precedence = 0,
-        isRightAssociative = false,
-        operateFunc = { left, right -> if(left == right) 1.0 else 0.0 }
-    )
-
-    object leq : Operator(
-        stringRepresentation = "<=",
-        precedence = 0,
-        isRightAssociative = false,
-        operateFunc = { left, right -> if (left <= right) 1.0 else 0.0 })
-
-    object geq : Operator(
-        stringRepresentation = ">=",
-        precedence = 0,
-        isRightAssociative = false,
-        operateFunc = { left, right -> if(left >= right) 1.0 else 0.0 })
-
-    object lt : Operator(
-        stringRepresentation = "<",
-        precedence = 0,
-        isRightAssociative = false,
-        operateFunc = { left, right -> if(left < right) 1.0 else 0.0 })
-
-    object gt : Operator(
-        stringRepresentation = ">",
-        precedence = 0,
-        isRightAssociative = false,
-        operateFunc = { left, right -> if(left > right) 1.0 else 0.0 })
-
-
-    object plus : Operator(
-        stringRepresentation = "+",
-        precedence = 1,
-        isRightAssociative = false,
-        operateFunc = { left, right -> left.plus(right) })
-
-    object minus : Operator(
-        stringRepresentation = "-",
-        precedence = 1,
-        isRightAssociative = false,
-        operateFunc = { left, right -> left.minus(right) })
-
-    object times : Operator(
-        stringRepresentation = "*",
-        precedence = 2,
-        isRightAssociative = false,
-        operateFunc = { left, right -> left.times(right) })
-
-    object div : Operator(
-        stringRepresentation = "/",
-        precedence = 2,
-        isRightAssociative = false,
-        operateFunc = { left, right -> left.div(right) })
-
-    object mod : Operator(
-        stringRepresentation = "%",
-        precedence = 2,
-        isRightAssociative = false,
-        operateFunc = { left, right -> left.rem(right) })
-
-    object pow : Operator(
-        stringRepresentation = "^",
-        precedence = 3,
-        isRightAssociative = true,
-        operateFunc = { left, right -> Math.pow(left, right) }
-    )
-
     companion object {
-        // 2
+        // 1
         val tokens = "><=\\^%*+\\-"
 
+        // 2
         fun fromString(representation:String) = when(representation) {
             eq.stringRepresentation -> eq
             leq.stringRepresentation -> leq
@@ -104,4 +34,87 @@ sealed class Operator(
     }
 
     override fun toString(): String = stringRepresentation
+
+    // 3
+    object eq : Operator(
+        stringRepresentation = "=",
+        precedence = 0,
+        isRightAssociative = false
+    )
+
+    object leq : Operator(
+        stringRepresentation = "<=",
+        precedence = 0,
+        isRightAssociative = false
+    )
+
+    object geq : Operator(
+        stringRepresentation = ">=",
+        precedence = 0,
+        isRightAssociative = false
+    )
+
+    object lt : Operator(
+        stringRepresentation = "<",
+        precedence = 0,
+        isRightAssociative = false
+    )
+
+    object gt : Operator(
+        stringRepresentation = ">",
+        precedence = 0,
+        isRightAssociative = false
+    )
+
+
+    object plus : Operator(
+        stringRepresentation = "+",
+        precedence = 1,
+        isRightAssociative = false
+    )
+
+    object minus : Operator(
+        stringRepresentation = "-",
+        precedence = 1,
+        isRightAssociative = false
+    )
+
+    object times : Operator(
+        stringRepresentation = "*",
+        precedence = 2,
+        isRightAssociative = false
+    )
+
+    object div : Operator(
+        stringRepresentation = "/",
+        precedence = 2,
+        isRightAssociative = false
+    )
+
+    object mod : Operator(
+        stringRepresentation = "%",
+        precedence = 2,
+        isRightAssociative = false
+    )
+
+    object pow : Operator(
+        stringRepresentation = "^",
+        precedence = 3,
+        isRightAssociative = true
+    )
+
+    // 4
+    fun operate(right:Double, left:Double):Double = when(this) {
+        is eq ->    if(left == right)   1.0 else 0.0
+        is leq ->   if (left <= right)  1.0 else 0.0
+        is geq ->   if(left >= right)   1.0 else 0.0
+        is lt ->    if(left < right)    1.0 else 0.0
+        is gt ->    if(left > right)    1.0 else 0.0
+        is plus ->  left.plus(right)
+        is minus -> left.minus(right)
+        is times -> left.times(right)
+        is div ->   left.div(right)
+        is mod ->   left.rem(right)
+        is pow ->   Math.pow(left, right)
+    }
 }
